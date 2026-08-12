09cc228e8bcca06057692336f74f6b3b3ab0b9c6
Fooxygen <FluentLiquidOxygen@outlook.com>
2026-08-12 16:07:29 +0800

    ✏️ 调整符号

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection } from 'astro:content';
 import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
+const blogs = defineCollection({
     loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
     schema: z.object({
         title: z.string(),
@@ -11,4 +11,4 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+export const collections = { blogs };
